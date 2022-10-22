dc706803c8745bc972d96cd24eb0640d4f3cef58
Lucas Guzman-Finn <lggufi@gmail.com>
2022-10-22 11:27:16 -0400

    half done confirmation

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,55 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './styles/contact.css';
+import Sent from '../components/Sent';
 
 export default function Contact() {
-  const [FN, setFN] = useState();
-  const [LN, setLN] = useState();
-  const [email, setEmail] = useState();
-  const [phone, setPhone] = useState();
-  const [comp, setComp] = useState();
-  const [help, setHelp] = useState();
-  const [hear, setHear] = useState()
+  const form = useRef();
+  const [ sending, setSending ] = useState(false)
+  const [ sent, setSent ] = useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setSending(true)
+    emailjs.sendForm('service_jz8ssw4', 'template_a65m2ac', form.current, 'gMV75W7yi5PsJBDjv')
+      .then((result) => {
+          console.log(result.text);
+          setSent(true)
+      }, (error) => {
+          console.log(error.text);
+          setSent(false)
+      });
+  };
 
   return (
     <>
-    <div className="content contact-content">
-      <h1><span style={{color: "#0A1D33"}}>Get In</span> <span style={{color: "#791233"}}>Touch</span></h1>
-      <h4>Ready to expedite your development journey? Have any questions on what we can do for you? Reach out! Let's get to know one another.</h4>
-      <form className="contact-form">
-        <div className="small">
-          <label>
-            First name
-            <input type="text" value={FN} onChange={(e) => {setFN(e.target.value)}}/>
+    <div className="content">
+      {sent ? <Sent/> : <div className='contact-content'>
+        <h1><span style={{color: "#0A1D33"}}>Get In</span> <span style={{color: "#791233"}}>Touch</span></h1>
+        <h4>Ready to expedite your development journey? Have any questions on what we can do for you? Reach out! Let's get to know one another.</h4>
+        <form ref={form} onSubmit={sendEmail} className="contact-form">
+          <div className="small">
+            <label>
+              First name
+              <input type="text" name='firstname' />
+            </label>
+            <label>
+              Last Name
+              <input type="text" name='lastname' />
+            </label>
+          </div>
+          <div className="small">
+            <label>
+              Email
+              <input type="email" name='email' />
+            </label>
+            <label>
+              Phone
+              <input type="tel" name='phone' />
+            </label>
+          </div>
+          <label className="med">
+            Company
+            <input type="text" name='comp' />
           </label>
-          <label>
-            Last Name
-            <input type="text" value={LN} onChange={(e) => {setLN(e.target.value)}}/>
+          <label className="large">
+            How Can We Help?
+            <textarea type="text" name='help' />
           </label>
-        </div>
-        <div className="small">
-          <label>
-            Email
-            <input type="email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+          <label className="large">
+            How Did You Hear About Us?
+            <textarea type="text" name='hear' />
           </label>
-          <label>
-            Phone
-            <input type="tel" value={phone} onChange={(e) => {setPhone(e.target.value)}}/>
-          </label>
-        </div>
-        <label className="med">
-          Company
-          <input type="text" value={comp} onChange={(e) => {setComp(e.target.value)}}/>
-        </label>
-        <label className="large">
-          How Can We Help?
-          <textarea type="text" value={help} onChange={(e) => {setHelp(e.target.value)}}/>
-        </label>
-        <label className="large">
-          How Did You Hear About Us?
-          <textarea type="text" value={hear} onChange={(e) => {setHear(e.target.value)}}/>
-        </label>
-        <button type="submit">Send</button>
-      </form>
+          <div className="sub">
+            <button type="submit">Send</button>
+            {sending ? 
+            <div className="loading">
+              <div className="top"/>
+              <div className="bottom"/>
+            </div>
+            : ''}
+          </div>
+        </form>
+      </div>}
     </div>
     </>
   )
