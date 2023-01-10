3fcca6282cc69b426e78044b8abcc4e324f3a9ca
Lucas Guzmán-Finn <lggufi@gmail.com>
2023-01-10 18:48:02 -0500

    hide part that needs confirmation, margins

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -125,7 +125,7 @@ export default function Home() {
       <div className='reviewsec'>
         <div className='reviews content'>
           <div className='rev-sec'>
-              <h1><span style={{color: "#FAF9F6"}}>What </span><span style={{color: "#0A1D33"}}>People </span><span style={{color: "#FAF9F6"}}>Have Said</span></h1>
+              <h1><span style={{color: "#FAF9F6"}}>What </span><span style={{color: "#748EC7"}}>People </span><span style={{color: "#FAF9F6"}}>Have Said</span></h1>
               <div className='rev-top-wave'>
                 <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
